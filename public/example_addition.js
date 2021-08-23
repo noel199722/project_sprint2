46ffc0c95e57cbf8905b1fc0763c6fe65d3fc41d
Noel <noel199722@gmail.com>
2021-08-23 16:33:32 +0800

    rm index

--- public/example_addition.js
+++
@@ -1,6 +0,0 @@
-function addUpdateDeleteButton() {
-    const dataDetailForm = document.querySelector('#data-detail-form .button')
-    dataDetailForm.addEventListener('click', (event) => {
-        console.log(event.target)
-    })
-}
